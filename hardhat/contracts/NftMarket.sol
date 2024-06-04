d41201f96e7fa3822c46f3af1161a8b62c53abe8
Kartik <sehrawatkar@gmail.com>
2024-06-04 11:41:05 +0530

    new features in smart contract like duplicate uri check, price of nft item,listed item count, fetching nft item and there test

--- hardhat/contracts/NftMarket.sol
+++ hardhat/contracts/NftMarket.sol
@@ -7,8 +7,43 @@ contract NftMarket is ERC721URIStorage {
 
   uint256 private _listedItems;
   uint256 private _tokenIds;
+  uint public listingPrice = 0.025 ether;
+
+  struct NftItem{
+  uint tokenId;
+  uint price;
+  address creator;
+  bool isListed;
+  }
+
+  mapping(string => bool) private _usedTokenURIs;
+  mapping(uint => NftItem) private _idtoNftItem;
+
+  event NftItemCreated(
+    uint tokenId,
+    uint price,
+    address creator,
+    bool islisted
+  );
+
   constructor() ERC721("CreaturesNFT", "CNFT") {}
-  function mintToken(string memory tokenURI) public payable returns(uint){
+
+  function getNftItem(uint tokenId) public view returns(NftItem memory){
+    return _idtoNftItem[tokenId];
+  }
+
+  function listedItemsCount() public view returns (uint){
+    return _listedItems;
+  }
+
+  function tokenURIExits(string memory tokenURI) public view returns(bool){
+    return _usedTokenURIs[tokenURI];
+  }
+
+  function mintToken(string memory tokenURI,uint price) public payable returns(uint){
+    require(!tokenURIExits(tokenURI),"Token URI already exists");
+    require(msg.value==listingPrice,"Price must be equal to listing Price");
+
     _tokenIds+=1;
     _listedItems+=1;
 
@@ -16,7 +51,17 @@ contract NftMarket is ERC721URIStorage {
 
     _safeMint(msg.sender,newTokenId);
     _setTokenURI(newTokenId,tokenURI);
+    _createNftItem(newTokenId,price);
+    _usedTokenURIs[tokenURI]=true;
 
     return newTokenId;
   }
+
+  function _createNftItem(uint tokenId, uint price) private {
+    require(price>0,"Price must be atleast 1 Wei");
+    _idtoNftItem[tokenId]=NftItem(tokenId,price,msg.sender,true);
+    emit NftItemCreated(tokenId,price,msg.sender,true);
+  }
+
+  
 }
--- hardhat/test/NftMarket_test.js
+++ hardhat/test/NftMarket_test.js
@@ -6,6 +6,8 @@ describe("NftMarket", function () {
   let owner;
   let addr1;
   let addr2;
+  let _nftPrice = ethers.parseEther("0.3").toString();
+  let _listingPrice = ethers.parseEther("0.025").toString();
 
   const deployedContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
@@ -20,7 +22,10 @@ describe("NftMarket", function () {
     const tokenURI = "https://testing.com";
 
     before(async function () {
-      await nftMarket.mintToken(tokenURI, { from: owner.address });
+      await nftMarket.mintToken(tokenURI, _nftPrice, {
+        from: owner.address,
+        value: _listingPrice,
+      });
     });
 
     it("owner of first token should be owner.address", async function () {
@@ -31,6 +36,32 @@ describe("NftMarket", function () {
     it("token id should be pointing to the given tokenURI", async function () {
       const actualtokenuri = await nftMarket.tokenURI(1);
       expect(actualtokenuri).to.equal(tokenURI);
+    });
+
+    it("should not be possible to create a NFT with used tokenURI", async function () {
+      try {
+        await nftMarket.mintToken(tokenURI, _nftPrice, {
+          from: owner.address,
+          value: _listingPrice,
+        });
+      } catch (e) {
+        console.log("Test Case Passed");
+        expect(e.message).to.include("Token URI already exists");
+      }
+    });
+
+    it("Should have one listed item", async () => {
+      const listedItems = await nftMarket.listedItemsCount();
+      expect(listedItems.toString()).to.equal("1");
+    });
+
+    it("should have create NFT item", async () => {
+      const nftItem = await nftMarket.getNftItem(1);
+
+      expect(nftItem.tokenId).to.equal(1);
+      expect(nftItem.price).to.equal(_nftPrice);
+      expect(nftItem.creator).to.equal(owner.address);
+      expect(nftItem.isListed).to.equal(true);
     });
   });
 });
