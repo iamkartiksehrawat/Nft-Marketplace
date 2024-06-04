391adb65ec0885147f6fdac29bd4f07ff70cb4cf
Kartik <sehrawatkar@gmail.com>
2024-06-04 21:07:21 +0530

    Changing the deploying function for hardhat

--- hardhat/ignition/modules/NftMarket.js
+++ hardhat/ignition/modules/NftMarket.js
@@ -1,6 +1,8 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("NftMarket", (m) => {
-  const NftMarket = m.contract("NftMarket");
+  const NftMarket = m.contract("NftMarket", [
+    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  ]);
   return { NftMarket };
 });
